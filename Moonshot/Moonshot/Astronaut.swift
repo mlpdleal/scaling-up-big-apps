36d126c0a9e00b2346002e607f1cb22a2ff6f1e9
mlpdleal <manoellealdeveloper@gmail.com>
2022-06-04 15:38:37 -0300

    Completed Day 38 of 100 Days of SwiftUI

---
+++ Moonshot/Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Manoel Leal on 04/06/22.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable{
+    let id: String
+    let name: String
+    let description: String
+}
---
+++ Moonshot/Moonshot/Mission.swift
@@ -0,0 +1,36 @@
+//
+//  Mission.swift
+//  Moonshot
+//
+//  Created by Manoel Leal on 04/06/22.
+//
+
+import Foundation
+
+
+
+struct Mission: Codable, Identifiable {
+    
+    struct CrewRole: Codable {
+        let name: String
+        let role: String
+    }
+    
+    let id: Int
+    let launchDate: Date?
+    let crew: [CrewRole]
+    let description: String
+    
+    var displayName: String {
+        "Apollo \(id)"
+    }
+    
+    var image: String {
+        "apollo\(id)"
+    }
+    
+    var formattedLaunchDate: String {
+        launchDate?.formatted(date: .abbreviated, time: .omitted) ?? "N/A"
+    }
+    
+}
